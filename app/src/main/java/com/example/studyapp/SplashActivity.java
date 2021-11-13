d11abb198a1720d88e6ed6698d3fbc73c800ab5b
Cole Horvat <56736430+ColeHorvat@users.noreply.github.com>
2021-11-12 23:22:42 -0800

    Login and Splash Activity made

--- app/src/main/java/com/example/studyapp/SplashActivity.java
+++ app/src/main/java/com/example/studyapp/SplashActivity.java
@@ -4,15 +4,11 @@ import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class LoginActivity extends AppCompatActivity {
-    
+public class SplashActivity extends AppCompatActivity {
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        //requestWindowFeature(Window.FEATURE_NO_TITLE);
-        getSupportActionBar().hide();
-        setContentView(R.layout.activity_login);
-
+        setContentView(R.layout.activity_splash);
     }
-
 }
