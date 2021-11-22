618f4e87ee78ac2c466c73ddc5e26a7e33a0329d
Cole Horvat <56736430+ColeHorvat@users.noreply.github.com>
2021-11-21 17:39:56 -0800

    Tweaked login test

--- app/src/androidTest/java/com/example/studyapp/ActivityInputOutputTest.java
+++ app/src/androidTest/java/com/example/studyapp/ActivityInputOutputTest.java
@@ -3,6 +3,8 @@ package com.example.studyapp;
 import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.action.ViewActions.click;
 import static androidx.test.espresso.action.ViewActions.typeText;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
 
 import androidx.test.ext.junit.rules.ActivityScenarioRule;
@@ -31,7 +33,7 @@ public class ActivityInputOutputTest {
     }
 */
     @Test
-    public void activityLaunch() {
+    public void loginTest() {
         onView(withId(R.id.splashButton)).perform(click());
         onView(withId(R.id.testButton)).perform(click());
 
@@ -41,6 +43,7 @@ public class ActivityInputOutputTest {
                 typeText("Timbit10"));
 
         onView(withId(R.id.loginButton)).perform(click());
+        onView(withId(R.id.create_button)).check(matches(isDisplayed()));
     }
 
 }
