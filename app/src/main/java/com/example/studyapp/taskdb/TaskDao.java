953c1ab1fa5581b732c286823352fa8c01e8d3cc
Cole Horvat <56736430+ColeHorvat@users.noreply.github.com>
2021-11-24 20:28:34 -0800

    Added TaskDao

--- app/src/main/java/com/example/studyapp/taskdb/TaskDao.java
+++ app/src/main/java/com/example/studyapp/taskdb/TaskDao.java
@@ -1,4 +1,21 @@
 package com.example.studyapp.taskdb;
 
+import androidx.lifecycle.LiveData;
+import androidx.room.Dao;
+import androidx.room.Insert;
+import androidx.room.Query;
+
+import java.util.List;
+
+@Dao
 public interface TaskDao {
+    @Insert
+    void insert(Task task);
+
+    @Query("DELETE FROM task_table")
+    void deleteAll();
+
+    @Query("SELECT * FROM task_table ORDER BY id ASC")
+    LiveData<List<Task>> getAllTasks();
+
 }
