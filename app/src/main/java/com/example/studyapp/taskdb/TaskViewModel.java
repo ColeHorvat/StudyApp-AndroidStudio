70c002a0e9c5f4384520691131d60f32508acce8
Cole Horvat <56736430+ColeHorvat@users.noreply.github.com>
2021-11-27 02:51:31 -0800

    Database now persisting

--- app/src/main/java/com/example/studyapp/taskdb/TaskViewModel.java
+++ app/src/main/java/com/example/studyapp/taskdb/TaskViewModel.java
@@ -5,9 +5,6 @@ import android.app.Application;
 import androidx.lifecycle.AndroidViewModel;
 import androidx.lifecycle.LiveData;
 
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
-
 import java.util.List;
 
 public class TaskViewModel extends AndroidViewModel {
@@ -26,8 +23,8 @@ public class TaskViewModel extends AndroidViewModel {
 
     public void insert(Task task) {
         mRepository.insert(task);
-        FirebaseDatabase remoteDB = FirebaseDatabase.getInstance("https://study-app-85759-default-rtdb.firebaseio.com/");
-        DatabaseReference myRef = remoteDB.getReference("Task - " + task.getId());
-        myRef.setValue(task);
+        //FirebaseDatabase remoteDB = FirebaseDatabase.getInstance("https://study-app-85759-default-rtdb.firebaseio.com/");
+        //DatabaseReference myRef = remoteDB.getReference("Task - " + task.getId());
+        //myRef.setValue(task);
     }
 }
